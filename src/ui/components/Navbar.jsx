2dc07bae92f17b9ea2e7bbf4be5576e882bbfe61
MartinezFabian <FabianMartinezDev@gmail.com>
2023-09-25 20:30:50 -0300

    Feat: Add Navbar.module.css for component style

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,13 +1,33 @@
 import { NavLink } from 'react-router-dom';
 
+import styles from './Navbar.module.css';
+
 export const Navbar = () => {
   return (
-    <nav>
-      <h1>CountryExplorer</h1>
+    <nav className={styles.nav}>
+      <h1 className={styles.nav__h1}>
+        <NavLink to="/" className={styles.nav__heading}>
+          CountryExplorer
+        </NavLink>
+      </h1>
 
-      <div>
-        <NavLink to="/">All countries</NavLink>
-        <NavLink to="/search">Search</NavLink>
+      <div className={styles.nav__container}>
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            isActive ? `${styles.nav__link} ${styles.active}` : styles.nav__link
+          }
+        >
+          All countries
+        </NavLink>
+        <NavLink
+          to="/search"
+          className={({ isActive }) =>
+            isActive ? `${styles.nav__link} ${styles.active}` : styles.nav__link
+          }
+        >
+          Search
+        </NavLink>
       </div>
     </nav>
   );
