e28d657c8a18275dc2dc8951a4628414a0547421
MartinezFabian <FabianMartinezDev@gmail.com>
2023-09-29 19:21:53 -0300

    Feat: Add SearchInput.module.css for component style

--- src/countries/components/SearchInput.jsx
+++ src/countries/components/SearchInput.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+import styles from './SearchInput.module.css';
+
 export const SearchInput = () => {
   const navigate = useNavigate();
 
@@ -21,14 +23,32 @@ export const SearchInput = () => {
   };
 
   return (
-    <form onSubmit={onSubmitForm}>
-      <input
-        type="text"
-        onChange={onChangeInput}
-        value={inputText}
-        placeholder="Type the name of the country you are looking for"
-        autoComplete="off"
-      />
+    <form onSubmit={onSubmitForm} className={styles.form}>
+      <div className={styles.form__container}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="#1d1d1d"
+          className={styles.form__icon}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+          />
+        </svg>
+
+        <input
+          type="text"
+          onChange={onChangeInput}
+          value={inputText}
+          placeholder="Search"
+          autoComplete="off"
+          className={styles.form__input}
+        />
+      </div>
     </form>
   );
 };
