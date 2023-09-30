25a80a0928d02c9e1f3edd06aa9d61199f05aa69
MartinezFabian <FabianMartinezDev@gmail.com>
2023-09-29 21:19:28 -0300

    Feat: Add functionality to show alert

--- src/countries/pages/SearchPage.jsx
+++ src/countries/pages/SearchPage.jsx
@@ -31,6 +31,29 @@ export const SearchPage = () => {
 
       {isLoading ? <span className={styles.loader}></span> : null}
 
+      {countries.length === 0 && q !== '' ? (
+        <div className={styles.alert}>
+          <div className={styles.alert__text}>
+            No encontramos resultados para <span className={styles.bold}>{q}</span>
+          </div>
+
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="#1d1d1d"
+            className={styles.icon}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15.182 16.318A4.486 4.486 0 0012.016 15a4.486 4.486 0 00-3.198 1.318M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z"
+            />
+          </svg>
+        </div>
+      ) : null}
+
       <div className={styles.grid}>
         {countries.map((country) => (
           <CountryCard key={country.key} name={country.name} flag={country.flag}></CountryCard>
